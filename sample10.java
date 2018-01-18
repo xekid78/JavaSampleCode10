b6eb419e8965998361fe3c4c620fe9480429af61
Makoto Tanabe <xekid78@outlook.com>
2018-01-18 11:37:28 +0900

    数値の標準入力

---
+++ sample10.java
@@ -0,0 +1,18 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+import java.util.Scanner;
+
+public class sample10 {
+
+	public static void main(String[] args) {
+		System.out.println("数値の入力をお願いします。");
+		Scanner scan = new Scanner(System.in);
+		int num = scan.nextInt();
+		System.out.println(num + "と入力しました。");
+
+	}
+
+}
