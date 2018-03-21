4c60d022c8e34a547ccc1e8004c1dcd9f24dad91
Makoto Tanabe <xekid78@outlook.com>
2018-03-21 16:00:58 +0900

    Add files via upload

---
+++ Stdinpnum.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @author xekid78
+ *
+ */
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Stdinpnum {
+
+	public static void main(String[] args) {
+		System.out.println("数値の入力をお願いします。");
+		try (Scanner scan = new Scanner(System.in)) {
+			int num = scan.nextInt();
+			System.out.println(num + "と入力しました。");
+		} catch (NoSuchElementException e) {
+			e.printStackTrace();
+		} catch (IllegalStateException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
